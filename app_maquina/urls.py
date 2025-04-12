a482c3f0c48be4b38a3be838bec68faeea8a8878
pedro vitor <pedrovitorrochang@gmail.com>
2025-04-12 03:25:09 -0300

    Remover arquivos da branch main

--- app_maquina/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from app_maquina import views
-
-urlpatterns = [
-    path("", views.Cadastrar_Maquina, name="cadastrar_maquina")
-]
--- app_pagina/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from app_pagina import views
-
-def incio(request):
-    return render(request, 'Screens/index.html')
-
